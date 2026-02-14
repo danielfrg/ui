386bec5da11a4344b40aabecd69052ef2bf1e389
Daniel Rodriguez <daniel@danielfrg.com>
2026-02-14 11:47:12 -0600

    feat: Add Select

--- packages/ui/src/utils/external.ts
+++ packages/ui/src/utils/external.ts
@@ -1,4 +1,5 @@
 export { createEventListener } from "@solid-primitives/event-listener";
+export { Key } from "@solid-primitives/keyed";
 export { mergeRefs } from "@solid-primitives/refs";
 export type { MaybeAccessor } from "@solid-primitives/utils";
 export { access, accessWith, chain, noop } from "@solid-primitives/utils";
--- packages/ui/src/utils/number.ts
+++ packages/ui/src/utils/number.ts
@@ -16,3 +16,49 @@ export function clamp(
 ): number {
 	return Math.min(Math.max(value, min), max);
 }
+
+export function snapValueToStep(
+	value: number,
+	min: number,
+	max: number,
+	step: number,
+): number {
+	const remainder = (value - (Number.isNaN(min) ? 0 : min)) % step;
+	let snappedValue =
+		Math.abs(remainder) * 2 >= step
+			? value + Math.sign(remainder) * (step - Math.abs(remainder))
+			: value - remainder;
+
+	if (!Number.isNaN(min)) {
+		if (snappedValue < min) {
+			snappedValue = min;
+		} else if (!Number.isNaN(max) && snappedValue > max) {
+			snappedValue = min + Math.floor((max - min) / step) * step;
+		}
+	} else if (!Number.isNaN(max) && snappedValue > max) {
+		snappedValue = Math.floor(max / step) * step;
+	}
+
+	// correct floating point behavior by rounding to step precision
+	const string = step.toString();
+	const index = string.indexOf(".");
+	const precision = index >= 0 ? string.length - index : 0;
+
+	if (precision > 0) {
+		const pow = 10 ** precision;
+		snappedValue = Math.round(snappedValue * pow) / pow;
+	}
+
+	return snappedValue;
+}
+
+export const getPrecision = (n: number) => {
+	let e = 1;
+	let precision = 0;
+	while (Math.round(n * e) / e !== n) {
+		e *= 10;
+		precision++;
+	}
+
+	return precision;
+};
